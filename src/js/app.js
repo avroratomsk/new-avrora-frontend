6ef195e7d1fbff146cabfe5e57ab3870d9cba03e
sanchyopas <saniagolovanev@gmail.com>
2024-12-06 12:50:33 +0300

    dev: main block and fixed contact

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,16 @@
 import * as functions from "./modules/functions.js";
 
 functions.isWebp();
+
+// import Swiper bundle with all modules installed
+import {EffectFade, Navigation, Pagination, Autoplay} from 'swiper/modules';
+import Swiper from 'swiper';
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation, Pagination, EffectFade, Autoplay],
+    effect: "fade",
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+});
